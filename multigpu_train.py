eebda38407162631cdabca7ed583691cef701fdd
andrgje <aftgjesdal@gmail.com>
2022-04-22 21:57:10 +0200

    some fixes to val tesnorboard??

--- multigpu_train.py
+++ multigpu_train.py
@@ -202,8 +202,8 @@ def main(argv=None):
                 avg_val_loss = loss/FLAGS.steps_per_epoch_val		
 
                 loss_placeholder = tf.placeholder(tf.float32, shape=[], name="validation_loss")
-                val_loss_summary_op = sess.run([tf.summary.scalar("validation_loss", loss_placeholder)], feed_dict={loss_placeholder: avg_val_loss})
-                summary_writer.add_summary(val_loss_summary_op, global_step=step)
+                val_loss_summary_op,val_summary_string = sess.run([tf.summary.scalar("validation_loss", loss_placeholder),summary_op], feed_dict={loss_placeholder: avg_val_loss})
+                summary_writer.add_summary(val_summary_string, global_step=step)
                 
                 if avg_val_loss< avg_val_loss_best:
                     avg_val_loss_best = avg_val_loss
@@ -212,7 +212,7 @@ def main(argv=None):
                     
                 best_val_count+=1
                 print("val loss: %.4f   best_val_loss: %.4f,  val count: %d" %(avg_val_loss, avg_val_loss_best, best_val_count)) 
-                if best_val_count>5:
+                if best_val_count>20:
                     print("Quit: Validation loss not decreased in 5 epochs")
                     quit()
 if __name__ == '__main__':
